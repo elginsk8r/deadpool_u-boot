a163675326e754e4e26868a746a083c9eb6e22ad
bumerc77 <bumerc.com@gmail.com>
2025-05-01 13:49:04 +0200

    board: amlogic: sm1_bananapim5_v1: Optimize pwm_voltage_table
    Problem: Some values ​​are not usable.
    
    bl301.dis:
    
    '1000d330 <pwm_voltage_table_ee_new>:
    1000d330: 00120000 andseq r0, r2, r0
    1000d334: 000002bc ; <UNDEFINED> instruction: 0x000002bc
    ...
    1000d3c8 <pwm_voltage_table_ee>:
    ...
    1000d3d0: 001b0001 andseq r0, fp, r1
    1000d3d4: 000002b3 ; <UNDEFINED> instruction: 0x000002b3'
    
    Solution: Clean up and reconstruct it.

--- board/amlogic/sm1_bananapim5_v1/firmware/scp_task/pwm_ctrl.h
+++ board/amlogic/sm1_bananapim5_v1/firmware/scp_task/pwm_ctrl.h
@@ -1,30 +1,11 @@
 /*
-* Copyright (C) 2017 Amlogic, Inc. All rights reserved.
-* *
-This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* *
-This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-* *
-You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc.,
-* 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-* *
-Description:
+*board/amlogic/txl_p321_v1/firmware/scp_task/pwm_vol_tab.h
+*table for Dynamic Voltage/Frequency Scaling
 */
-
 #ifndef __PWM_CTRL_H__
 #define __PWM_CTRL_H__
 
 static int pwm_voltage_table_ee[][2] = {
-	{ 0x1c0000,  681},
-	{ 0x1b0001,  691},
-	{ 0x1a0002,  701},
 	{ 0x190003,  711},
 	{ 0x180004,  721},
 	{ 0x170005,  731},
@@ -48,30 +29,26 @@ static int pwm_voltage_table_ee[][2] = {
 	{ 0x050017,  912},
 	{ 0x040018,  922},
 	{ 0x030019,  932},
-	{ 0x02001a,  942},
-	{ 0x01001b,  952},
-	{ 0x00001c,  962}
+	{ 0x02001a,  942}
 };
 
 static int pwm_voltage_table_ee_new[][2] = {
-	{ 0x120000,  700},
-	{ 0x110001,  710},
-	{ 0x100002,  720},
-	{ 0x0f0003,  730},
-	{ 0x0e0004,  740},
-	{ 0x0d0005,  750},
-	{ 0x0c0006,  760},
-	{ 0x0b0007,  770},
-	{ 0x0a0008,  780},
-	{ 0x090009,  790},
-	{ 0x08000a,  800},
-	{ 0x07000b,  810},
-	{ 0x06000c,  820},
-	{ 0x05000d,  830},
-	{ 0x04000e,  840},
-	{ 0x03000f,  850},
-	{ 0x020010,  860},
-	{ 0x010011,  870},
-	{ 0x000012,  880},
+	{ 0x100002,  730},
+	{ 0x0f0003,  740},
+	{ 0x0e0004,  750},
+	{ 0x0d0005,  760},
+	{ 0x0c0006,  770},
+	{ 0x0b0007,  780},
+	{ 0x0a0008,  790},
+	{ 0x090007,  800},
+	{ 0x080008,  810},
+	{ 0x070009,  820},
+	{ 0x06000a,  830},
+	{ 0x05000b,  840},
+	{ 0x04000c,  850},
+	{ 0x03000d,  860},
+	{ 0x02000e,  870},
+	{ 0x01000f,  880},
+	{ 0x000010,  890},
 };
 #endif //__PWM_CTRL_H__
