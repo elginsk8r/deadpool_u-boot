88eb5d0dde290031fa2da5e5ae5c29c3a98b0ad4
bumerc77 <bumerc.com@gmail.com>
2025-05-14 11:19:39 +0200

    board: amlogic: sm1_bananapim5_v1: Rewrite USB3-HUB init
    * Improved solution.

--- board/amlogic/sm1_bananapim5_v1/sm1_bananapim5_v1.c
+++ board/amlogic/sm1_bananapim5_v1/sm1_bananapim5_v1.c
@@ -620,10 +620,27 @@ int board_init(void)
 	set_i2c_ao_pinmux();
 #endif
 
-/* USB3-HUB init */
-run_command("gpio set GPIOH_4", 0);
-udelay(100);
-run_command("gpio set GPIOH_6", 0);
+/* BPI-M5 power on USB3-HUB */
+unsigned int val;
+	val = readl(PREG_PAD_GPIO3_EN_N);
+	val &= ~(1<<6);
+	writel(val, PREG_PAD_GPIO3_EN_N);
+	printf("gpio: GPIOH_6 usb power-on\n");
+
+	val = readl(PERIPHS_PIN_MUX_C);
+	val &= (~(0xf));
+	writel(val, PERIPHS_PIN_MUX_C);
+
+	udelay(100);
+
+	val = readl(PREG_PAD_GPIO3_EN_N);
+	val &= ~(1<<4);
+	writel(val, PREG_PAD_GPIO3_EN_N);
+	printf("gpio: GPIOH_4 usb reset\n");
+
+	val = readl(PERIPHS_PIN_MUX_C);
+	val &= (~(0xf));
+	writel(val, PERIPHS_PIN_MUX_C);
 
 	return 0;
 }
