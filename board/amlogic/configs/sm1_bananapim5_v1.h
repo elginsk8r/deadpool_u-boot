0e254a67017fabd982d3a593638f12f6c79e0f87
bumerc77 <bumerc.com@gmail.com>
2025-05-25 18:34:25 +0200

    board: amlogic: sm1_bananapim5_v1: Add Wi-Fi module (HAT) check
    Background:
    * The M5 board (without Wi-Fi module) has a boot issue during LOS initial setup.
    * This fails because the Wi-Fi module is enabled by default via the dtbo overlay.
    
    Solution:
    * Check the presence of the Wi-Fi module
    * and use the dtbo overlay according to the test status
    * DTBO key 0 = android_p_overlay_dt
    * DTBO key 1 = sm1_s905y3_bananapim5_wifi_overlay

--- board/amlogic/configs/sm1_bananapim5_v1.h
+++ board/amlogic/configs/sm1_bananapim5_v1.h
@@ -335,6 +335,13 @@
                 "echo detect recovery key; run recovery_from_flash;"\
             "fi;"\
             "\0"\
+            "\0"\
+        "wifi_module_check="\
+            "if gpio input GPIOX_6; then "\
+                "echo M5: no wifi; keyman write dtbo str 0; "\
+            "else "\
+                "echo M5: has wifi; keyman write dtbo str 1; "\
+            "fi;fi;"\
 	"irremote_update="\
 		"if irkey 2500000 0xe31cfb04 0xb748fb04; then "\
 			"echo read irkey ok!; " \
@@ -351,6 +358,7 @@
             "run factory_reset_poweroff_protect;"\
             "run upgrade_check;"\
             "run init_display;"\
+            "run wifi_module_check;"\
             "run storeargs;"\
             "run recovery_key;" \
             "bcb uboot-command;"\
